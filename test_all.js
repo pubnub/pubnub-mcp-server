543e16e9fdb55ed62eb10a07ceb215173d68e790
Stephen Blum <stephen@pubnub.com>
2025-06-10 14:42:09 -0700

    added check for failure output in test_all.

--- test_all.js
+++ test_all.js
@@ -10,12 +10,33 @@ if (!OPENAI_API_KEY) {
 function runCommand(cmd, args, options = {}) {
   return new Promise((resolve, reject) => {
     console.log(`\n> ${cmd} ${args.join(' ')}`);
-    const proc = spawn(cmd, args, { stdio: 'inherit', shell: false, ...options });
+    const proc = spawn(cmd, args, { shell: false, stdio: ['inherit', 'pipe', 'pipe'], ...options });
+    let failed = false;
+    proc.stdout.on('data', (data) => {
+      const text = data.toString();
+      process.stdout.write(text);
+      if (!failed && /fail/i.test(text)) {
+        failed = true;
+        const msg = `Detected 'fail' in stdout of ${cmd} ${args.join(' ')}`;
+        reject(new Error(msg));
+        proc.kill();
+      }
+    });
+    proc.stderr.on('data', (data) => {
+      const text = data.toString();
+      process.stderr.write(text);
+      if (!failed && /fail/i.test(text)) {
+        failed = true;
+        const msg = `Detected 'fail' in stderr of ${cmd} ${args.join(' ')}`;
+        reject(new Error(msg));
+        proc.kill();
+      }
+    });
     proc.on('error', reject);
     proc.on('exit', (code) => {
-      if (code === 0) {
+      if (code === 0 && !failed) {
         resolve();
-      } else {
+      } else if (code !== 0) {
         reject(new Error(`${cmd} ${args.join(' ')} exited with code ${code}`));
       }
     });
